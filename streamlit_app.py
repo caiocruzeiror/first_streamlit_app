d5b9d68886808fa1c22c4bdafa4f6a6cb3348333
caiocruzeiror <62437733+caiocruzeiror@users.noreply.github.com>
2022-09-15 16:00:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
 
-streamlit.header('Lorem Ipsum')
-line = 'Whole wound wrote at whose to style in.\n'\
-  'Figure ye innate former do so we. Shutters but sir yourself provided you required his.\n'\
-  'So neither related he am do believe. Nothing but you hundred had use regular. Fat sportsmen arranging preferred can.\n'\
-  'Busy paid like is oh. Dinner our ask talent her age hardly. Neglected collected an attention listening do abilities.'
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.text(line)
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
