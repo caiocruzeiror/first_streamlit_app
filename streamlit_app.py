38d595d744a0f188a820589fb71b0697c0a4868a
caiocruzeiror <62437733+caiocruzeiror@users.noreply.github.com>
2022-09-15 15:55:53 +0100

    Update streamlit_app.py
    fix text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Lorem Ipsum')
-streamlit.text('Ever man are put down his very. And marry may table him avoid.
-               'Hard sell it were into it upon. He forbade affixed parties of assured to me windows.
-               'Happiness him nor she disposing provision.
-               'Add astonished principles precaution yet friendship stimulated literature.
-               'State thing might stand one his plate. Offending or extremity therefore so difficult he on provision.
-               'Tended depart turned not are.')
+line = 'Whole wound wrote at whose to style in.'\
+  'Figure ye innate former do so we. Shutters but sir yourself provided you required his.'\
+  'So neither related he am do believe. Nothing but you hundred had use regular. Fat sportsmen arranging preferred can.'\
+  'Busy paid like is oh. Dinner our ask talent her age hardly. Neglected collected an attention listening do abilities.'
+
+streamlit.text(line)
