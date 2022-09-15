f61c85782b250e12975fb3017d110284e69a268a
caiocruzeiror <62437733+caiocruzeiror@users.noreply.github.com>
2022-09-15 15:56:29 +0100

    Update streamlit_app.py
    breakline?

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Lorem Ipsum')
-line = 'Whole wound wrote at whose to style in.'\
-  'Figure ye innate former do so we. Shutters but sir yourself provided you required his.'\
-  'So neither related he am do believe. Nothing but you hundred had use regular. Fat sportsmen arranging preferred can.'\
+line = 'Whole wound wrote at whose to style in.'\n
+  'Figure ye innate former do so we. Shutters but sir yourself provided you required his.'\n
+  'So neither related he am do believe. Nothing but you hundred had use regular. Fat sportsmen arranging preferred can.'\n
   'Busy paid like is oh. Dinner our ask talent her age hardly. Neglected collected an attention listening do abilities.'
 
 streamlit.text(line)
